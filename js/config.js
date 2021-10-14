519a11ff1e686e37c2d7f07c975121aa4c442e02
captain <captain@CaptaindeMacBook-Pro-Office-2.local>
2021-10-14 16:46:53 +0800

    modify config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_fY0GYB0of', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4d4s2bfbm3qq1etjh5th0q292f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
